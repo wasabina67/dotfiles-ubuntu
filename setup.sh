f5776706b0788659c2f71eb0f66e0d142068d31d
wasabina67 <wasabina67@gmail.com>
2024-06-24 11:52:46 +0900

    Update setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FILES=(
+    "bin"
+    ".bash_logout"
+    ".bashrc"
+    ".gitconfig"
+    ".profile"
+)
+DOTFILES_DIR="/home/wasabina67/dotfiles-ubuntu"
+
+for f in "${FILES[@]}"; do
+    if [ ! -e "${HOME}/${f}" ]; then
+        ln -s "${DOTFILES_DIR}/${f}" "${HOME}/${f}"
+    fi
+done
